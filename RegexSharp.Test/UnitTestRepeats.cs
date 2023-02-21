5f2c75baf4a8d82618cb47ceb3c7002643b0c782
Davide Messori <davide.messori@outlook.com>
2023-02-21 18:13:19 +0100

    Add and, or, repetitions

--- RegexSharp.Test/UnitTestRepeats.cs
+++ RegexSharp.Test/UnitTestRepeats.cs
@@ -5,18 +5,100 @@ namespace RegexSharp.Test;
 
 public class UnitTestRepeat
 {
-    [Fact]
-    public void MatchRange()
+    [Theory]
+    [InlineData("abcabc")]
+    [InlineData("abcabcabc")]
+    public void MatchRange_ShouldMatch_AGivenStringRepeatedRangeTimes(string input)
     {
         var regex = new RegexBuilder()
-            .Exact("abc")[1..3]
+            .Exact("abc")[2..3]
             .At.LineStartEnd()
             .Build();
 
-        Assert.False(regex.Match("cba").Success);
-        Assert.True(regex.Match("abc").Success);
-        Assert.True(regex.Match("abcabc").Success);
-        Assert.True(regex.Match("abcabcabc").Success);
-        Assert.False(regex.Match("abcabcabcabc").Success);
+        var match = regex.Match(input);
+
+        Assert.True(match.Success);
     }
+
+    [Theory]
+    [InlineData("abc")]
+    [InlineData("abcabcabcabc")]
+    public void MatchRange_ShouldNotMatch_AGivenStringRepeatedRangeTimes(string input)
+    {
+        var regex = new RegexBuilder()
+            .Exact("abc")[2..3]
+            .At.LineStartEnd()
+            .Build();
+
+        var match = regex.Match(input);
+
+        Assert.False(match.Success);
+    }
+
+    [Theory]
+    [InlineData("abcd")]
+    [InlineData("abc")]
+    public void MatchRange_ShouldMatch_AGivenStringOptionally(string input)
+    {
+        var regex = (new RegexBuilder()
+            .Exact("abc")
+            .And(RegexBuilder.FromString("d").Optionally()))
+            .At.LineStartEnd()
+            .Build();
+
+        var match = regex.Match(input);
+
+        Assert.True(match.Success);
+    }
+
+    [Theory]
+    [InlineData("abcd")]
+    [InlineData("abcb")]
+    [InlineData("abc")]
+    public void MatchRange_ShouldMatch_AGivenStringInOrOptionally(string input)
+    {
+        var regex = (new RegexBuilder()
+            .Exact("abc")
+            .And(RegexBuilder.FromString("d").Or("b").Optionally()))
+            .At.LineStartEnd()
+            .Build();
+
+        var match = regex.Match(input);
+
+        Assert.True(match.Success);
+    }
+    
+    [Theory]
+    [InlineData("ciao@google.com")]
+    [InlineData("ciaone@yahoo.studenti.it")]
+    [InlineData("lol@libero.it")]
+    public void Test_ShouldMatch_EmailAdresses(string input)
+    {
+        var regex = (RegexBuilder
+            .FromString(".*") + "@" +
+            RegexBuilder.FromString("google").Or("yahoo").Or("libero") +
+            RegexBuilder.FromString("\\.studenti").Optionally() +
+            RegexBuilder.FromString("\\.it").Or("\\.com"))
+            .At.LineStartEnd()
+            .Build();
+
+        var match = regex.Match(input);
+
+        Assert.True(match.Success);
+    }
+
+    // [Theory]
+    // [InlineData("abc")]
+    // [InlineData("abcabcabcabc")]
+    // public void MatchRange_ShouldNotMatch_AGivenStringOptionally(string input)
+    // {
+    //     var regex = new RegexBuilder()
+    //         .Exact("abc")[2..3]
+    //         .At.LineStartEnd()
+    //         .Build();
+
+    //     var match = regex.Match(input);
+
+    //     Assert.False(match.Success);
+    // }
 }
--- RegexSharp/Builders/RegexBuilder.cs
+++ RegexSharp/Builders/RegexBuilder.cs
@@ -13,16 +13,68 @@ public class RegexBuilder : BaseRegexBuilder
         _builder = new StringBuilder();
     }
 
+    public RegexBuilder(string s) : base(new StringBuilder(s)) {}
+
     public RegexBuilder Exact(string s)
     {
         _builder.Append(s);
         return this;
     }
 
+    public RegexBuilder And(RegexBuilder rb)
+    {
+        _builder.Append(rb._builder);
+        return this;
+    }
+
+    public RegexBuilder Or(RegexBuilder rb)
+    {
+        SurroundGroup();
+        rb.SurroundGroup();
+
+        _builder.Append($"|{rb._builder}");
+        return this;
+    }
+
+    public static RegexBuilder FromString(string s) => new RegexBuilder(s);
+
+    public RegexBuilder Optionally()
+    {
+        SurroundGroup();
+        _builder.Append("?");
+        return this;
+    }
+
+    public RegexBuilder OneOrMore()
+    {
+        SurroundGroup();
+        _builder.Append("+");
+        return this;
+    }
+
+    public RegexBuilder AnyTimes()
+    {
+        SurroundGroup();
+        _builder.Append("*");
+        return this;
+    }
+
     public static implicit operator string(RegexBuilder rb)
     {
         return rb._builder.ToString() ?? "";
-    }  
+    }
+
+    public static implicit operator RegexBuilder(string str)
+    {
+        var builder = new RegexBuilder();
+        return builder.Exact(str);
+    }
+
+    public static RegexBuilder operator +(RegexBuilder a, RegexBuilder b)
+    {
+        a._builder.Append(b._builder);
+        return a;
+    }
 
     public RegexBuilder this[Range range]
     {
@@ -30,6 +82,16 @@ public class RegexBuilder : BaseRegexBuilder
         {
             SurroundGroup();
             _builder.Append($"{{{range.Start.Value},{range.End.Value}}}");
+            return this;
+        }
+    }
+
+    public RegexBuilder this[int times]
+    {
+        get
+        {
+            SurroundGroup();
+            _builder.Append($"{{{times}}}");
             return this;
         }
     }
